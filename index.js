1000b12be8142395d08ae192bcb7735280f98ebe
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-04-19 16:21:03 +0100

    Adapt for Node.

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-/**
- * For MW environment. Grab all "Gizmek Inari, the Grain Storage Fox" targets
- * (https://yugipedia.com/wiki/Gizmek_Inari,_the_Grain_Storage_Fox)
- * @author Becasita
- */
-async function getSameATKDEF() {
-	const STATS = [
-		0, 100, 150, 200, 250, 300, 350, 400, 450, 500,550, 600, 650, 666, 700, 750, 800, 850, 900, 920, 950,
-		1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1380, 1400, 1450, 1460, 1500, 1530, 1550, 1600, 1610, 1650, 1700, 1750, 1800, 1850, 1900, 1930, 1950,
-		2000, 2050, 2100, 2150, 2200, 2250, 2300, 2350, 2400, 2450, 2500, 2510, 2550, 2600, 2650, 2700, 2750, 2800, 2850, 2900, 2950,
-		3000, 3100, 3200, 3300, 3400, 3450, 3500, 3600, 3700, 3750, 3800,
-		4000, 4200, 4400, 4500, 4600,
-		5000
-	];
+const { promises: fsp } = require( 'fs' );
 
+const fetch = require( 'node-fetch' );
+
+const STATS = require( './stats' );
+
+const { dumpFile } = require( './config' );
+
+function makeQuery( stat ) {
+	const query = `[[Medium::TCG||OCG]][[ATK string::${stat}]][[DEF string::${stat}]][[Belongs to::Main Deck]][[Summoning::Can be Special Summoned]]|?Attribute|?Type|?Stars string|?Summoning|?Primary type|limit=500`;
+
+	return `https://yugipedia.com/api.php?action=ask&query=${encodeURIComponent( query )}&format=json`;
+}
+
+async function updateHtmlPage( monsters ) {
+	const content = await fsp.readFile( dumpFile, 'utf8' );
+
+	const updatedContent = content.replace(
+		/(?<=^[ \t]*const[ \t]+YUGIPEDIA_DATA[ \t]+=[ \t]+)(.*?)(; ?)(.*?)$/gm,
+		`${JSON.stringify( monsters )}$2// Last updated at: ${new Date().toISOString()}`,
+	);
+
+	return fsp.writeFile( dumpFile, updatedContent );
+}
+
+( async () => {
 	const monsters = [];
 
-	const api = await mw.loader.using( 'mediawiki.api' ).then( () => new mw.Api() );
-
 	for ( const stat of STATS ) {
-		const response = await api.get( {
-			action: 'ask',
-			query: `[[Medium::TCG||OCG]] [[ATK string::${stat}]] [[DEF string::${stat}]] [[Belongs to::Main Deck]] |?Attribute|?Type|?Stars string|?Summoning|?Primary type| limit = 500`,
-		} );
+		const response = await fetch( makeQuery( stat ) ).then( r => r.json() );
 
 		Object.entries( response.query.results ).forEach( ( [ monster, { printouts } ] ) => {
 			if (
-				printouts.Summoning.some( c => c.match( /nomi\b/i ) )
+				monster.match( '(original)' )
 				||
-				printouts[ 'Primary type' ].some( t => t.fulltext.match( /ritual/i ) )
-			) {
-				console.log( 'Filtering', monster );
+				printouts[ 'Primary type' ].some( t => t.fulltext.match( /ritual/i ) )	) {
+//				console.log( 'Filtering', monster );
 
 				return;
 			}
-			
-			console.log( 'Pushing', monster );
-		
+
+//			console.log( 'Pushing', monster );
+
 			monsters.push( {
 				name: monster,
 				atkDef: stat,
@@ -47,7 +52,10 @@ async function getSameATKDEF() {
 		} );
 	}
 
-	console.log( 'Done' );
+	return monsters;
+} )()
+	.then( updateHtmlPage )
+	.then( () => console.log( 'Done' ) )
+	.catch( console.error )
+;
 
-	window[ `__monsters${Date.now().toString( 36 )}` ] = monsters;
-}
